5b73f137642f2fa70a8158c2a012c56240b79226
Bruno Skvorc <bruno@bitfalls.com>
2019-08-14 18:05:34 +0200

    Fixes #4

--- README.md
+++ README.md
@@ -23,16 +23,23 @@ The results of the build process will be in `docgen/.vuepress/dist`.
 
 > Note: You might have to run `vuepress build` twice, because the first run writes the temporary MD files, while the second run builds HTML from them. We're working on making all that happen in one go and there's a [pending issue in Vuepress](https://github.com/vuejs/vuepress/issues/1613).
 
+## Commands
+
+- `vuepress build` builds the docs
+- `yarn run clearlib` clears the downloaded and generated MD files in `lib`
+- `yarn run cleardist` clears the generated static files
+- `yarn run clearall` clears all the cached content - both MD files and static file
+
 ## What's behind the build command
 
 When you run `vuepress build`, the builder:
 
 - reads `config.json` for the repos it should process.
 - uses the information there to build the homepage by constructing library cards for each repo.
-- for every repo with `update: true`, grabs their `README` file and strips their header and footer (above `Introduction` and below `Contributing`).
+- for every repo with `update: true`, grabs their `README` file and strips their header and footer (above `Introduction` and below `Contributing`). It is recommended you fix your repos to match this pattern rather than use `update: false`.
 - changes image URLs to match raw ones from Github if they are hosted on Github.
 - generates frontmatter from the data in `config.json`, combines it with the README and generates a homepage for each library that way.
-- if a `Guides` folder exists in library's subfolder, it will generate a sidebar navigation from its contents.
+- @todo: if a `Guides` folder exists in library's subfolder, it will generate a sidebar navigation from its contents.
 
 The logic responsible for this is in a custom plugin in `.vuepress/docgen/plugin.js`.
 
@@ -46,15 +53,26 @@ To generate docs in the same way for your own repos:
     - `location`: the Github URL of the repo. Must be public. Gitlab and private repos coming soon.
     - `update`: when false, only generates content from local MD content, does not try to fetch from online master
     - `tags`: a JS array of tags applying to this lib. Purely aesthetic for now, for the homepage - colored badges will appear next to the lib's name. Add tags into the `tags` object as desired.
+    - `subdocs`: see next section
     - `description`: description for the homepage
     - `frontmatter`: frontmatter to generate. Key value pairs. Values are same as [documented in Vuepress](https://v1.vuepress.vuejs.org/guide/frontmatter.html).
 2. Also in `config.json`, set up the start and end separators. This indicates where your README's body begins, and where it ends. Useful for avoiding licensing information or CI badges in your human-readable docs. `separators[0]` is the starting point of the readme's body, `separators[1]` is the ending point of the readme's body, and `separators[2]` lets you specify several separators for start and end if your READMEs across projects aren't standardized. The string of each separator will be exploded with `separators[2]` and the first of those which is found in a README is considered the valid separator.
 3. Modify styles in `.vuepress/styles` and theme configuration in `.vuepress/config.js` as desired. Use the Vuepress docs.
 4. Run `vuepress build` inside `docgen`.
 
+### Subdocs
+
+Sometimes your master README file might contain links to subdocs of that repo. 
+
+Those subdocs are assumed to be listed in a subsection with links e.g. `"subdocs": "## Docs"`. That label will be used as a section identifier for the subdocs. All markdown links in that section will be grabbed in their raw format - the generator will assume they are all subdocs, so don't have other links in there.
+
+If these subdocs have H1 level headings, all headings will be moved 1 level lower (i.e. `##` becomes `###`). The subsection will be replaced with a content composed of all subdocs merged into a single file.
+
+@todo currently image URLs are not fixed because it's [not straightforward](https://github.com/status-im/nimbus-docs-suite/issues/6).
+
 ## Enhancing the docs further
 
-To further enhance the docs, please consult the [Vuepress docs](https://v1.vuepress.vuejs.org) as underneath it's all just a [Vue](https://vuejs.org) app built by Vuepress.
+To further enhance the docs, please consult the [Vuepress docs](https://v1.vuepress.vuejs.org) as underneath it's all just a [Vue](https://vuejs.org) app built by Vuepress. Also check [existing issues](https://github.com/status-im/nimbus-docs-suite/issues).
 
 ## License
 
--- docgen/.vuepress/docgen/plugin.js
+++ docgen/.vuepress/docgen/plugin.js
@@ -4,26 +4,6 @@ const request = require('request');
 const fs = require('fs');
 
 module.exports = {
-    extendCli (cli) {
-
-
-
-        
-        cli
-          .command('foocommand [targetDir]', '')
-          .option('--debug', 'display info in debug mode')
-          .action((dir = '.') => {
-            console.log('Display info of your website')
-          });
-
-        cli
-          .command('barcommand [targetDir]', '')
-          .option('--debug', 'display info in debug mode')
-          .action((dir = '.') => {
-            console.log('Display info of your website 2')
-          });
-    },
-
     ready () {
         console.log("Initializing library docs fetching");
 
@@ -63,61 +43,103 @@ module.exports = {
             let repoPath = repos[i].location.replace(/\/?$/, '/');
             let rawPath = repoPath.replace("https://github.com", "https://raw.githubusercontent.com");
             let readmePath = rawPath + "master/README.md";
-            request.get(readmePath, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    let content = body;
 
-                    let ss, es;
-                    for (let ssLen = 0; ssLen < startSeparators.length; ssLen++) {
-                        if (content.indexOf(startSeparators[ssLen]) > -1) {
-                            ss = startSeparators[ssLen];
-                            break;
-                        }
-                    }
-                    for (let esLen = 0; esLen < endSeparators.length; esLen++) {
-                        if (content.indexOf(endSeparators[esLen]) > -1) {
-                            es = endSeparators[esLen];
-                            break;
-                        }
-                    }
+            processReadme(readmePath);
 
-                    let readmeBody = content.split(ss)[1];
-                    readmeBody = "# " + repos[i].label + "\n\n" + readmeBody.split(es)[0];
+            function processReadme(path) {
 
-                    console.log("Fixing images");
-                    // Apply only to local images in repo
-                    readmeBody = readmeBody.replace(/\!\[(.*)\]\((?!http)(.*)\)/igm, function (match, g1, g2) {
-                        return "![" + g1 + "](" + repos[i].location.replace(/\/?$/, '/')+"raw/master/" + g2 + "?sanitize=true)";
-                    });
-
-                    let frontMatter = "";
-                    if (repos[i].frontMatter !== undefined) {
-                        for (let key in repos[i].frontMatter) {
-                            if (repos[i].frontMatter.hasOwnProperty(key)) {
-                                frontMatter += key + ": " + repos[i].frontMatter[key] + "\n";
+                console.log("Fetching " + readmePath);
+                request.get(readmePath, async function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        console.log("File fetched successfully");
+                        let content = body;
+    
+                        let ss, es;
+                        for (let ssLen = 0; ssLen < startSeparators.length; ssLen++) {
+                            if (content.indexOf(startSeparators[ssLen]) > -1) {
+                                ss = startSeparators[ssLen];
+                                break;
                             }
                         }
-                        frontMatter = "---\n" + frontMatter + "---\n\n";
-                    }
-
-                    let finalFile = frontMatter + readmeBody;
-
-                    var dir = './lib/'+repos[i].name;
-
-                    if (!fs.existsSync(dir)){
-                        fs.mkdirSync(dir);
-                    }
-
-                    console.log("Writing " + dir+"/README.md");
-                    fs.writeFileSync(dir+"/README.md", finalFile, function(err) {
-                        if(err) {
-                            return console.log(err);
+                        for (let esLen = 0; esLen < endSeparators.length; esLen++) {
+                            if (content.indexOf(endSeparators[esLen]) > -1) {
+                                es = endSeparators[esLen];
+                                break;
+                            }
                         }
-                    
-                        console.log("The file " + dir + "/README.md" + " was saved!");
-                    }); 
-                }
-            });
+    
+                        let readmeBody = content.split(ss)[1];
+                        readmeBody = "# " + repos[i].label + "\n\n" + readmeBody.split(es)[0];
+
+                        console.log("Fixing images");
+                        // Apply only to local images in repo
+                        readmeBody = readmeBody.replace(/\!\[(.*)\]\((?!http)(.*)\)/igm, function (match, g1, g2) {
+                            return "![" + g1 + "](" + repos[i].location.replace(/\/?$/, '/')+"raw/master/" + g2 + "?sanitize=true)";
+                        });
+
+                        if (repos[i].subdocs !== undefined && readmeBody.indexOf(repos[i].subdocs) > -1) {
+                            console.log("Subdocs detected in README. Parsing nested docs.");
+                            // Grab the whole section, from subdocs label to next newline starting with # (new subheading)
+                            let subSection = readmeBody.split(repos[i].subdocs)[1];
+                            subSection = subSection.split(/^##\s.*/gmi)[0];
+
+                            // console.log("-----------------");
+                            // console.log(readmeBody);
+                            readmeBody = readmeBody.replace(repos[i].subdocs + subSection, "--subdocs--");
+                            // console.log(readmeBody);
+                            // console.log(readmeBody.indexOf(subSection));
+                            // console.log("-----------------");
+
+                            let rex = /\((.*\.md)\)/gmi;
+                            let match = rex.exec(subSection);
+                            //let matches = [];
+                            let subdocsContent = "";
+                            while (match != null) {
+                                //matches.push(match[1]);
+                                // Got all the links, let's fetch them.
+                                // console.log("Fetchable link " + readmePath.replace("README.md", match[1]));
+                                let subdoc = await downloadPage(readmePath.replace("README.md", match[1]));
+                                match = rex.exec(subSection);
+
+                                // Deepen heading level for all subheadings if H1 detected
+                                subdoc = deepenHeadings(subdoc);
+
+                                subdocsContent += subdoc;
+                            }
+
+                            //console.log(subdocsContent);
+                            readmeBody = readmeBody.replace("--subdocs--", subdocsContent);
+                        }
+    
+                        let frontMatter = "";
+                        if (repos[i].frontMatter !== undefined) {
+                            for (let key in repos[i].frontMatter) {
+                                if (repos[i].frontMatter.hasOwnProperty(key)) {
+                                    frontMatter += key + ": " + repos[i].frontMatter[key] + "\n";
+                                }
+                            }
+                            frontMatter = "---\n" + frontMatter + "---\n\n";
+                        }
+    
+                        let finalFile = frontMatter + readmeBody;
+    
+                        var dir = './lib/'+repos[i].name;
+    
+                        if (!fs.existsSync(dir)){
+                            fs.mkdirSync(dir);
+                        }
+    
+                        console.log("Writing " + dir+"/README.md");
+                        fs.writeFileSync(dir+"/README.md", finalFile, function(err) {
+                            if(err) {
+                                return console.log(err);
+                            }
+                        
+                            console.log("The file " + dir + "/README.md" + " was saved!");
+                        }); 
+                    }
+                });                
+            }
 
         }
 
@@ -131,4 +153,27 @@ module.exports = {
             console.log("The main README.md file was saved!");
         }); 
     }     
+}
+
+function downloadPage(url) {
+    return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (error) reject(error);
+            if (response.statusCode != 200) {
+                reject('Invalid status code <' + response.statusCode + '>');
+            }
+            resolve(body);
+        });
+    });
+}
+
+function deepenHeadings(content) {
+    // Detect if H1 exists
+    let h1rex = /^#\s.*/gmi;
+    if (h1rex.test(content)) {
+        // Increase all headings by one
+        console.log("Subdoc has H1 heading, pushing all headings one level down");
+        content = content.replace(/^#/gmi, "##");
+    }
+    return content;
 }
